2599e80d099202e8951ce7d0b0e4b65d7a7168b1
xupengfei <faberxu@gmail.com>
2022-09-15 17:25:39 +0800

    替换mysql-plus
    Former-commit-id: 088f8fa76a841990670ad81cef6f56e2a3e6db3c [formerly e56326e07143eaaf100252f6793b5815cf51a274]
    Former-commit-id: c3a0939d401c1bb4857946f11d744fd4b3ba3fc3

--- faber-admin/src/main/java/com/faber/AdminBootstrap.java
+++ faber-admin/src/main/java/com/faber/AdminBootstrap.java
@@ -3,6 +3,7 @@ package com.faber;
 import com.ace.cache.EnableAceCache;
 import com.faber.admin.util.logs.DBLog;
 import com.spring4all.swagger.EnableSwagger2Doc;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.server.ConfigurableWebServerFactory;
---
+++ faber-admin/src/main/java/com/faber/demo/mapper/DemoStudentMapper.java
@@ -0,0 +1,8 @@
+package com.faber.demo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.faber.demo.entity.Student;
+
+public interface DemoStudentMapper extends BaseMapper<Student> {
+
+}
