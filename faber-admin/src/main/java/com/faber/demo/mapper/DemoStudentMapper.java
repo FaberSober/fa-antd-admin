3e9bed5f36e8ab8e045eee0a4bbacf0b2f15204b
xupengfei <faberxu@gmail.com>
2022-09-16 09:14:26 +0800

    MyBatis-Plus
    Former-commit-id: 5e573fa8aeb8789195ab931add89d85a450cc0c7 [formerly 8acc4af3682c73c576c7063c9019902ba3948d86]
    Former-commit-id: 08736f22413ff69f3826776236b88cc2b590e0ce

--- faber-admin/src/main/java/com/faber/demo/mapper/DemoStudentMapper.java
+++
@@ -1,8 +0,0 @@
-package com.faber.demo.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.faber.demo.entity.Student;
-
-public interface DemoStudentMapper extends BaseMapper<Student> {
-
-}
