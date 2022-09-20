793335edf82303b7c73fc1d3b611b13ddf82a7a4
xupengfei <faberxu@gmail.com>
2022-09-20 17:27:12 +0800

    代码整理
    Former-commit-id: df46d5fb569bd8561aa32d36b4d2356d9d1f90e5 [formerly 1646455e8ef516c8a4d86795ab36bb84a889cafc]
    Former-commit-id: 8f4c0f8756f6c77efb3b4b8f56e910a2607973f1

--- faber-admin/src/main/java/com/faber/rbac/biz/RbacUserRoleBiz.java
+++ faber-admin/src/main/java/com/faber/rbac/biz/RbacUserRoleBiz.java
@@ -1,9 +1,19 @@
 package com.faber.rbac.biz;
 
 import com.faber.common.biz.BaseBiz;
+import com.faber.rbac.entity.RbacMenu;
+import com.faber.rbac.entity.RbacRole;
+import com.faber.rbac.entity.RbacRoleMenu;
 import com.faber.rbac.entity.RbacUserRole;
 import com.faber.rbac.mapper.RbacUserRoleMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * BASE-用户角色关联表
@@ -14,4 +24,37 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class RbacUserRoleBiz extends BaseBiz<RbacUserRoleMapper,RbacUserRole> {
+
+    @Autowired
+    private RbacRoleBiz rbacRoleBiz;
+
+    @Autowired
+    private RbacRoleMenuBiz rbacRoleMenuBiz;
+
+    @Autowired
+    private RbacMenuBiz rbacMenuBiz;
+
+    public List<Long> getMyRoleIds() {
+        List<RbacUserRole> userRoleList = lambdaQuery().eq(RbacUserRole::getUserId, getCurrentUserId()).list();
+        return userRoleList.stream().map(RbacUserRole::getRoleId).collect(Collectors.toList());
+    }
+
+    public List<RbacRole> getMyRoles() {
+        List<Long> roleIds = this.getMyRoleIds();
+        if (roleIds.isEmpty()) return new ArrayList<>();
+
+        return rbacRoleBiz.lambdaQuery().in(RbacRole::getId, roleIds).list();
+    }
+
+    public List<RbacMenu> getMyRoleMenus() {
+        List<Long> roleIds = this.getMyRoleIds();
+        if (roleIds.isEmpty()) return new ArrayList<>();
+
+        List<RbacRoleMenu> roleMenuList = rbacRoleMenuBiz.lambdaQuery().in(RbacRoleMenu::getRoleId, roleIds).list();
+        List<Long> menuIds = roleMenuList.stream().map(RbacRoleMenu::getMenuId).collect(Collectors.toList());
+        if (menuIds.isEmpty()) return new ArrayList<>();
+
+        return rbacMenuBiz.lambdaQuery().in(RbacMenu::getId, menuIds).list();
+    }
+
 }
--- faber-admin/src/main/java/com/faber/rbac/rest/RbacUserRoleController.java
+++ faber-admin/src/main/java/com/faber/rbac/rest/RbacUserRoleController.java
@@ -1,10 +1,14 @@
 package com.faber.rbac.rest;
 
+import com.faber.common.msg.ObjectRestResponse;
 import com.faber.common.rest.BaseController;
 import com.faber.rbac.biz.RbacUserRoleBiz;
+import com.faber.rbac.entity.RbacMenu;
+import com.faber.rbac.entity.RbacRole;
 import com.faber.rbac.entity.RbacUserRole;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * BASE-用户角色关联表
@@ -16,5 +20,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/rbac/rbacUserRole")
 public class RbacUserRoleController extends BaseController<RbacUserRoleBiz,RbacUserRole, Long> {
+
+    /**
+     * 获取登录账户的角色列表
+     * @return
+     */
+    @RequestMapping(value = "/getMyRoles", method = RequestMethod.GET)
+    @ResponseBody
+    public ObjectRestResponse<List<RbacRole>> getMyRoles() {
+        List<RbacRole> o = baseBiz.getMyRoles();
+        return ok(o);
+    }
+
+    /**
+     * 获取登录账户的权限列表
+     * @return
+     */
+    @RequestMapping(value = "/getMyRoleMenus", method = RequestMethod.GET)
+    @ResponseBody
+    public ObjectRestResponse<List<RbacMenu>> getMyRoleMenus() {
+        List<RbacMenu> o = baseBiz.getMyRoleMenus();
+        return ok(o);
+    }
 
 }
