57013ea3a74e6f52c046b93ac93bb6e6f2c8a45a
xupengfei <faberxu@gmail.com>
2022-10-10 15:47:45 +0800

    代码整理
    Former-commit-id: bd80eb8a6bab4aada544717a7d00238f92fbc505 [formerly 248ed9d40482fbfd59b759454d4c8fb1367b06ae]
    Former-commit-id: ccabdc3cc923b62bcdf05a319eecb4c5bc00611b

--- faber-admin/src/main/java/com/faber/admin/config/filter/RequestAgainFilter.java
+++ faber-admin/src/main/java/com/faber/admin/config/filter/RequestAgainFilter.java
@@ -32,7 +32,7 @@ public class RequestAgainFilter implements Filter {
     /**
      * 列表的api不记录日志
      */
-    private static final List<String> NO_LOG_APIS = Arrays.asList("/api/admin/logApi/page");
+    private static final List<String> NO_LOG_APIS = Arrays.asList("/api/admin/logApi/page", "api/admin/logLogin/page");
 
     @Autowired
     private LogApiBiz logApiBiz;
