fd19bfd2fe4a9ba67d99483195089890d8dca9b4
xupengfei <faberxu@gmail.com>
2022-10-17 16:36:57 +0800

    数据监控-druid
    Former-commit-id: e3a5fb4bcd5cc0aea99ccce75b9a3158d1ad6c66 [formerly f044edacd5ee129a33ffb975fb6bcb2d48624a27]
    Former-commit-id: e508161b07d0dc208402c1bf6d50484393e7bf30

---
+++ faber-admin/src/main/frontend/src/pages/admin/system/monitor/druid.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+/**
+ * @author xu.pengfei
+ * @date 2022/10/17
+ */
+export default function Druid() {
+  return (
+    <div className="fa-full-content">
+      <iframe src="/druid/index.html" style={{ border: 'none', width: '100%', height: '100%' }} />
+    </div>
+  )
+}
