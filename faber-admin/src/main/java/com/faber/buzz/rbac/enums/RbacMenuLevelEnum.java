ef561ddb5920456bccd925ad6519f14e2a3b7ed8
xupengfei <faberxu@gmail.com>
2022-11-28 17:42:26 +0800

    V1.0.3.sql
    Former-commit-id: ae5556f3a48fe08eb6b03382a3719917672bcbb5 [formerly ad1a355706f129f636d66ff272ff81026d33ae49]
    Former-commit-id: c199edc6c332894031e31298818d6d7967a5af99

--- faber-admin/src/main/java/com/faber/buzz/rbac/enums/RbacMenuLevelEnum.java
+++ faber-admin/src/main/java/com/faber/buzz/rbac/enums/RbacMenuLevelEnum.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
 
 @Getter
-//@JSONType(serializeEnumAsJavaBean = true)
 public enum RbacMenuLevelEnum implements IEnum<Integer> {
     APP(0, "模块"),
     MENU(1, "菜单"),
@@ -22,11 +21,4 @@ public enum RbacMenuLevelEnum implements IEnum<Integer> {
         this.desc = desc;
     }
 
-//    @JsonValue
-//    public Map<String,Object> toMap() {
-//        Map<String,Object> map = new HashMap<>();
-//        map.put("code", this.code);
-//        map.put("val ", this.val);
-//        return map;
-//    }
 }
