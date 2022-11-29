9af451b6514fbb7825ab5156568fe0b606a503ef
xupengfei <faberxu@gmail.com>
2022-11-29 10:23:34 +0800

    代码整理
    Former-commit-id: e36a886e32f20a1eb4f8098ce04c69216e9502e7 [formerly 8463036b2f2e35fa581b3f450dbf15411319bb6a]
    Former-commit-id: 6bb0d018f43cdea5dee8263b9525bd7589f491cc

--- faber-admin/src/main/frontend/src/layout/menu/cube/Logo.tsx
+++ faber-admin/src/main/frontend/src/layout/menu/cube/Logo.tsx
@@ -3,6 +3,8 @@ import {Link} from "react-router-dom";
 import styles from "@/layout/menu/MenuLayout.module.less";
 import {UserLayoutContext} from "@/layout/UserLayout";
 import {SITE_INFO} from "@/configs/server.config";
+import fileSaveApi from '@/services/admin/fileSave'
+
 
 /**
  * @author xu.pengfei
@@ -12,7 +14,7 @@ export default function Logo() {
   const { systemConfig } = useContext(UserLayoutContext)
   return (
     <Link to={SITE_INFO.HOME_LINK} className={styles.logo}>
-      <img src={systemConfig.logo} alt="logo" className={styles.logoImg} />
+      <img src={fileSaveApi.genLocalGetFile(systemConfig.logo)} alt="logo" className={styles.logoImg} />
       <span className={styles.logoTitle}>
         {systemConfig.title}
       </span>
