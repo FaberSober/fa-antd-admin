a58099ce02664611143e735db8565ac9977187f2
xu.pengfei <1508075252@qq.com>
2022-11-30 21:52:42 +0800

    级联选择
    Former-commit-id: f37c48677891a913e304bec5ed01ba63be759804 [formerly bf2ca79a21ad02a8ab82eec83022f4f69e639647]
    Former-commit-id: df0b878831e3ff0f7391407de12d3dbcb1da81c3

--- faber-admin/src/main/frontend/src/pages/admin/home/biz/cascader.tsx
+++ faber-admin/src/main/frontend/src/pages/admin/home/biz/cascader.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Card} from "antd";
-import DictTypeCascade from "@/pages/admin/system/base/dict/helper/DictTypeCascade";
+import TreeCascade from "@/pages/admin/demo/tree/helper/TreeCascade";
 
 
 /**
@@ -13,15 +13,15 @@ export default function cascader() {
   return (
     <div className="fa-full-content fa-padding-12">
       <Card title="级联选择-带一个根节点" style={{ marginBottom: 12 }}>
-        <p>说明：1. 从接口获取数据；2. 远程数据返回Tree型数据；3. 示例为字典分组选择；</p>
+        <p>说明：1. 从接口获取数据；2. 远程数据返回Tree型数据；</p>
 
-        <DictTypeCascade />
+        <TreeCascade />
       </Card>
 
       <Card title="级联选择-不带根节点" style={{ marginBottom: 12 }}>
-        <p>说明：1. 从接口获取数据；2. 远程数据返回Tree型数据；3. 示例为字典分组选择；</p>
+        <p>说明：1. 从接口获取数据；2. 远程数据返回Tree型数据；</p>
 
-        <DictTypeCascade showRoot={false} />
+        <TreeCascade showRoot={false} />
       </Card>
 
     </div>
