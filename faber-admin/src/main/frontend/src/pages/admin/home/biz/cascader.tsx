7b3454511f2e8d325cba2d97644a325263535bf4
xupengfei <faberxu@gmail.com>
2022-11-30 17:42:07 +0800

    级联选择
    Former-commit-id: 209a7b718256f07b1a97e2e147e63023d4fa473b [formerly 83eae218e43ab58e1bda92955f4c3f7a2d58f426]
    Former-commit-id: 45f66da74fa07255349b93125152b151c84d1c91

---
+++ faber-admin/src/main/frontend/src/pages/admin/home/biz/cascader.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Card} from "antd";
+import DictTypeCascade from "@/pages/admin/system/base/dict/helper/DictTypeCascade";
+
+
+/**
+ * 级联选择
+ * @author xu.pengfei
+ * @date 2022/11/7
+ */
+export default function cascader() {
+
+  return (
+    <div className="fa-full-content fa-padding-12">
+      <Card title="级联选择-带一个根节点" style={{ marginBottom: 12 }}>
+        <p>说明：1. 从接口获取数据；2. 远程数据返回Tree型数据；3. 示例为字典分组选择；</p>
+
+        <DictTypeCascade />
+      </Card>
+
+      <Card title="级联选择-不带根节点" style={{ marginBottom: 12 }}>
+        <p>说明：1. 从接口获取数据；2. 远程数据返回Tree型数据；3. 示例为字典分组选择；</p>
+
+        <DictTypeCascade showRoot={false} />
+      </Card>
+
+    </div>
+  )
+
+}
