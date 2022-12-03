3c9668a2a9f1b07542588ba2ef7fe47371b4f90e
xu.pengfei <1508075252@qq.com>
2022-12-03 18:28:58 +0800

    移除react-sortable-hoc依赖
    Former-commit-id: cc010643e0d7f9d8ecadcd240f41c484dfe22454 [formerly fabb51b1674127b5a68f9bfbd95fff1b204e7551]
    Former-commit-id: cbdee5cffe7d55e76aae630cda389415092aaddb

--- faber-admin/src/main/frontend/src/components/base-drag/index.ts
+++ faber-admin/src/main/frontend/src/components/base-drag/index.ts
@@ -1,8 +1,5 @@
-import FaDragHandle from './FaDragHandle'
-import FaSortableContainer from './FaSortableContainer'
-import FaSortableItem from './FaSortableItem'
 import FaDraggable from './FaDraggable'
 import FaSortItem from './FaSortItem'
 import FaSortList from './FaSortList'
 
-export { FaDragHandle, FaSortableContainer, FaSortableItem, FaDraggable, FaSortItem, FaSortList }
+export { FaDraggable, FaSortItem, FaSortList }
