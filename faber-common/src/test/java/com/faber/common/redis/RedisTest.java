672bfe0fdcf9b90a2e4f9dbd872bef42f2ef8b86
xupengfei <faberxu@gmail.com>
2022-12-07 15:06:06 +0800

    redis
    Former-commit-id: d0e1bfef1a2ec495851710ec88c0be628ff54fd6 [formerly af00fc0538f437e274d3f27ce11dadbc037fbff7]
    Former-commit-id: 9296bff67c4e441e02d0de14b98df4e10e484be4

--- faber-common/src/test/java/com/faber/common/redis/RedisTest.java
+++ faber-common/src/test/java/com/faber/common/redis/RedisTest.java
@@ -21,7 +21,7 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 public class RedisTest {
 
-    private RedissonClient client;
+    private RedissonClient redisson;
 
     protected RedissonClient getRedissonClient() {
         // 1. Create config object
@@ -35,36 +35,32 @@ public class RedisTest {
 
     @Before
     public void init() {
-        client = getRedissonClient();
+        redisson = getRedissonClient();
     }
 
     @Test
     public void testGetConfig() throws IOException {
-        Config config = client.getConfig();
+        Config config = redisson.getConfig();
         String yaml = config.toYAML();
         System.out.println(yaml);
     }
 
     @Test
-    public void testAtomicLong() {
-        RAtomicLong myLong = client.getAtomicLong("myLong");
-        myLong.set(88888888L);
-        RFuture<Boolean> isSet = myLong.compareAndSetAsync(6, 27);
-        isSet.handle((result, exception) -> {
-            // handle the result or exception here.
-            System.out.println(result);
-            return result;
-        });
-    }
-
-    @Test
     public void testString() {
-        RSetCache<String> cache = client.getSetCache("anyStr");
-        cache.add("foo");
+        RBucket<String> bucket = redisson.getBucket("myBucket");
+        bucket.setAsync("Hello");
+        log.info(bucket.get());
     }
 
     @Test
-    public void testHello() {
+    public void testObj() {
+        RBucket<CustomMessage> bucket = redisson.getBucket("myBucketObj");
+        bucket.setAsync(new CustomMessage("hello 123"));
+        log.info(bucket.get().toString());
+    }
+
+    @Test
+    public void testMap() {
         RedissonClient client = getRedissonClient();
         RMap<String, String> map = client.getMap("anyMap");
         map.put("foo", "bar1");
@@ -73,7 +69,21 @@ public class RedisTest {
     }
 
     @Test
-    public void testHello1() {
+    public void testKeys() {
+        RKeys keys = redisson.getKeys();
+
+        Iterable<String> allKeys = keys.getKeys();
+        allKeys.forEach(log::debug);
+
+        Iterable<String> foundedKeys = keys.getKeysByPattern("my*");
+        long numOfDeletedKeys = keys.delete("obj1", "obj2", "obj3");
+        long deletedKeysAmount = keys.deleteByPattern("test?");
+        String randomKey = keys.randomKey();
+        long keysAmount = keys.count();
+    }
+
+    @Test
+    public void testLong() {
         RedissonClient client = getRedissonClient();
         RAtomicLong rAtomicLong = client.getAtomicLong("anyLong");
         rAtomicLong.set(222222);
@@ -85,7 +95,7 @@ public class RedisTest {
         CountDownLatch countDownLatch = new CountDownLatch(1);
 
         {
-            RTopic topic = client.getTopic("anyTopic");
+            RTopic topic = redisson.getTopic("anyTopic");
             topic.addListener(CustomMessage.class, new MessageListener<CustomMessage>() {
                 @Override
                 public void onMessage(CharSequence charSequence, CustomMessage customMessage) {
@@ -97,7 +107,7 @@ public class RedisTest {
 
         // 在其他线程或JVM节点
         {
-            RTopic topic = client.getTopic("anyTopic");
+            RTopic topic = redisson.getTopic("anyTopic");
             long clientsReceivedMessage = topic.publish(new CustomMessage("hello"));
         }
 
