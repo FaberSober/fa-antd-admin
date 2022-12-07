fbb998f03d33f081e724295bc6453645b636185e
xupengfei <faberxu@gmail.com>
2022-12-07 14:35:54 +0800

    redis
    Former-commit-id: 95c73aa4b06f282c4917e026e85bcddf00f7147e [formerly 92e0cb5947bd25cdbca3deadce571782662f0bfd]
    Former-commit-id: 944e9fe5c6bdea9d43d314fb6be1662958c55951

--- faber-common/src/test/java/com/faber/common/redis/RedisTest.java
+++ faber-common/src/test/java/com/faber/common/redis/RedisTest.java
@@ -1,21 +1,27 @@
 package com.faber.common.redis;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
 import org.redisson.Redisson;
-import org.redisson.api.RAtomicLong;
-import org.redisson.api.RMap;
-import org.redisson.api.RStream;
-import org.redisson.api.RedissonClient;
+import org.redisson.api.*;
+import org.redisson.api.listener.MessageListener;
 import org.redisson.config.Config;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.time.Duration;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author xupengfei
  * @email faberxu@gmail.com
  * @date 2022/12/6 17:00
  */
+@Slf4j
 public class RedisTest {
+
+    private RedissonClient client;
 
     protected RedissonClient getRedissonClient() {
         // 1. Create config object
@@ -27,11 +33,34 @@ public class RedisTest {
         return Redisson.create(config);
     }
 
+    @Before
+    public void init() {
+        client = getRedissonClient();
+    }
+
     @Test
-    public void testGetConfig() {
-        RedissonClient client = getRedissonClient();
+    public void testGetConfig() throws IOException {
         Config config = client.getConfig();
-        System.out.println(config);
+        String yaml = config.toYAML();
+        System.out.println(yaml);
+    }
+
+    @Test
+    public void testAtomicLong() {
+        RAtomicLong myLong = client.getAtomicLong("myLong");
+        myLong.set(88888888L);
+        RFuture<Boolean> isSet = myLong.compareAndSetAsync(6, 27);
+        isSet.handle((result, exception) -> {
+            // handle the result or exception here.
+            System.out.println(result);
+            return result;
+        });
+    }
+
+    @Test
+    public void testString() {
+        RSetCache<String> cache = client.getSetCache("anyStr");
+        cache.add("foo");
     }
 
     @Test
@@ -49,6 +78,31 @@ public class RedisTest {
         RAtomicLong rAtomicLong = client.getAtomicLong("anyLong");
         rAtomicLong.set(222222);
         rAtomicLong.expire(Duration.ofSeconds(30));
+    }
+
+    @Test
+    public void testTopic() throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+
+        {
+            RTopic topic = client.getTopic("anyTopic");
+            topic.addListener(CustomMessage.class, new MessageListener<CustomMessage>() {
+                @Override
+                public void onMessage(CharSequence charSequence, CustomMessage customMessage) {
+                    log.info("topic received: " + customMessage);
+                    countDownLatch.countDown();
+                }
+            });
+        }
+
+        // 在其他线程或JVM节点
+        {
+            RTopic topic = client.getTopic("anyTopic");
+            long clientsReceivedMessage = topic.publish(new CustomMessage("hello"));
+        }
+
+        boolean b = countDownLatch.await(10, TimeUnit.SECONDS);
+        log.info("b: " + b);
     }
 
 }
