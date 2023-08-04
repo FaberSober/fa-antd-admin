10952e42ad36dded1df994a1ed74461be7f8196d
xupengfei <faberxu@gmail.com>
2023-08-04 15:27:08 +0800

    base-代码合并
    Former-commit-id: 447e4e7f487be5bb70411714c7b1d18c30244709

--- frontend/apps/admin/src/cubes/homecubes.ts
+++ frontend/apps/admin/src/cubes/homecubes.ts
@@ -1,3 +1,4 @@
 // 首页-全部用户通用的
 export * from '@features/fa-admin-pages/cubes/homecubes';
+export * from '@features/fa-admin-pages/cubes/monitorcubes';
 // export * from '@features/fa-admin-demo-pages/homecubes';
