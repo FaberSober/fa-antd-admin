a72bd49a5577fdc90d5537223e15f94b1f218501
xu.pengfei <1508075252@qq.com>
2025-08-19 22:10:29 +0800

    flow-WorkFlow Designer

--- frontend/apps/admin/features/fa-flow-pages/components/flow/nodes/AddNode.tsx
+++ frontend/apps/admin/features/fa-flow-pages/components/flow/nodes/AddNode.tsx
@@ -1,15 +1,50 @@
 import React from 'react';
+import { Flow, FlowEnums } from "@features/fa-flow-pages/types";
+import { Button, Popover } from 'antd';
+import { PlusOutlined } from "@ant-design/icons";
+import { FaIcon } from "@fa/icons";
 
 
 export interface AddNodeProps {
+  /** 流程配置节点Node JSON */
+  node: Flow.Node;
 }
 
 /**
  * @author xu.pengfei
  * @date 2025/8/19 21:03
  */
-export default function AddNode({}: AddNodeProps) {
+export default function AddNode({node}: AddNodeProps) {
+
+  function addType(type: FlowEnums.NodeType) {}
+
   return (
-    <div></div>
+    <div className="add-node-btn-box">
+      <div className="add-node-btn">
+        <Popover
+          content={(
+            <div className="add-node-popover-body fa-flex-row">
+              <div className="fa-flex-column-center fa-hover fa-p6" onClick={() => addType(FlowEnums.NodeType.approval)}>
+                <Button shape="circle" icon={<FaIcon icon="fa-solid fa-stamp" style={{color: '#ff943e'}} />} />
+                <div>审批节点</div>
+              </div>
+              <div className="fa-flex-column-center fa-hover fa-p6" onClick={() => addType(FlowEnums.NodeType.cc)}>
+                <Button shape="circle" icon={<FaIcon icon="fa-solid fa-stamp" style={{color: '#3296fa'}} />} />
+                <div>抄送节点</div>
+              </div>
+              <div className="fa-flex-column-center fa-hover fa-p6">
+                <Button shape="circle" icon={<FaIcon icon="fa-solid fa-stamp" style={{color: '#15BC83'}} />} />
+                <div>条件分支</div>
+              </div>
+            </div>
+          )}
+          title="添加节点"
+          trigger="click"
+          placement="rightTop"
+        >
+          <Button shape="circle" icon={<PlusOutlined />} />
+        </Popover>
+      </div>
+    </div>
   )
 }
--- frontend/apps/admin/features/fa-flow-pages/components/flow/nodes/Promoter.tsx
+++ frontend/apps/admin/features/fa-flow-pages/components/flow/nodes/Promoter.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { Flow } from "@features/fa-flow-pages/types";
+import { FaIcon } from '@fa/icons';
+import { BaseDrawer } from "@fa/ui";
+import AddNode from './AddNode';
+import { Input } from "antd";
 
 
 export interface PromoterProps {
@@ -12,9 +16,42 @@ export interface PromoterProps {
  * @date 2025/8/19 20:22
  */
 export default function Promoter({ node }: PromoterProps) {
+
+  function show() {
+
+  }
+
+
+  function toText(nodeConfig: Flow.Node){
+    if(nodeConfig.nodeAssigneeList && nodeConfig.nodeAssigneeList.length > 0){
+      return nodeConfig.nodeAssigneeList.map(item=>item.name).join("、")
+    }else{
+      return "所有人"
+    }
+  }
+
   return (
-    <div>
-      启动节点
+    <div className="node-wrap">
+      <BaseDrawer
+        triggerDom={(
+          <div className="node-wrap-box start-node" onClick={show}>
+            <div className="title" style={{ background: '#576a95' }}>
+              <FaIcon icon="fa-solid fa-user-large" />
+              <span>{node.nodeName}</span>
+            </div>
+            <div className="content">
+              <span>{toText(node)}</span>
+            </div>
+          </div>
+        )}
+        title={(
+          <Input value={node.nodeName} variant="filled" />
+        )}
+      >
+        drawer
+      </BaseDrawer>
+
+      <AddNode node={node} />
     </div>
   )
 }
