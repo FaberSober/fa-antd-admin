331d969eedd1bd42c163d7a64c2905cee47b28c8
xu.pengfei <1508075252@qq.com>
2025-08-19 17:29:02 +0800

    demo-flow base

---
+++ frontend/apps/admin/features/fa-flow-pages/pages/admin/demo/flow/base/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+/**
+ * demo flow base editor
+ * @author xu.pengfei
+ * @date 2025/8/19 17:27
+ */
+export default function DemoFlowBase() {
+  return (
+    <div>
+      <div>demo flow base editor</div>
+    </div>
+  )
+}
