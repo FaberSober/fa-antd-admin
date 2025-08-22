63f06f93432457c8b0a2c70af35ec2ffa5e1fd70
xu.pengfei <1508075252@qq.com>
2025-08-22 11:04:59 +0800

    demo-flow-WorkFlow Designer-Branch.tsx

--- frontend/apps/admin/features/fa-flow-pages/pages/admin/demo/flow/base/index.tsx
+++ frontend/apps/admin/features/fa-flow-pages/pages/admin/demo/flow/base/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { FaFlexRestLayout } from "@fa/ui";
 import { FaWorkFlow } from "@features/fa-flow-pages/components";
 import { DEMO_FLOW_CONFIG } from './example';
 
@@ -11,13 +12,15 @@ export default function DemoFlowBase() {
   const [processModel, setProcessModel] = useState(DEMO_FLOW_CONFIG)
 
   return (
-    <div>
+    <div className="fa-full fa-flex-column">
       <div>demo flow base editor</div>
 
-      <FaWorkFlow
-        processModel={processModel}
-        onChange={setProcessModel}
-      />
+      <FaFlexRestLayout>
+        <FaWorkFlow
+          processModel={processModel}
+          onChange={setProcessModel}
+        />
+      </FaFlexRestLayout>
     </div>
   )
 }
