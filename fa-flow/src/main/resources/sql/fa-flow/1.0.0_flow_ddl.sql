a4c40ee9ae990af7e99492df29134fefaadf618d
xupengfei <faberxu@gmail.com>
2025-08-24 11:38:07 +0800

    flow-sql

--- fa-flow/src/main/resources/sql/fa-flow/1.0.0_flow_ddl.sql
+++ fa-flow/src/main/resources/sql/fa-flow/1.0.0_flow_ddl.sql
@@ -7,9 +7,9 @@ SET FOREIGN_KEY_CHECKS = 0;
 
 CREATE TABLE `flow_catagory` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
-  `parent_id` int(11) DEFAULT NULL COMMENT '上级节点',
+  `parent_id` int(11) NOT NULL COMMENT '上级节点',
   `name` varchar(255) NOT NULL COMMENT '名称',
-  `sort` int(11) DEFAULT NULL COMMENT '排序ID',
+  `sort` int(11) DEFAULT '0' COMMENT '排序ID',
   `icon` varchar(255) DEFAULT NULL COMMENT '图标',
   `crt_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `crt_user` varchar(32) NOT NULL COMMENT '创建用户ID',
@@ -26,8 +26,19 @@ CREATE TABLE `flow_catagory` (
 
 CREATE TABLE `flow_process` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
-  `catagory_id` int(11) DEFAULT NULL COMMENT '分类ID',
-
+  `catagory_id` int(11) NOT NULL COMMENT '流程分类ID',
+  `process_id` bigint(20) DEFAULT NULL COMMENT '当前流程ID',
+  `process_key` varchar(100) DEFAULT NULL COMMENT '流程定义 key 唯一标识',
+  `process_name` varchar(100) NOT NULL COMMENT '名称',
+  `process_icon` varchar(255) DEFAULT NULL COMMENT '图标',
+  `process_type` varchar(100) DEFAULT NULL COMMENT '类型',
+  `process_version` int(11) NOT NULL DEFAULT '1' COMMENT '流程版本，默认 1',
+  `instance_url` varchar(200) DEFAULT NULL COMMENT '实例地址',
+  `remark` varchar(255) DEFAULT NULL COMMENT '备注说明',
+  `use_scope` tinyint(1) NOT NULL DEFAULT '0' COMMENT '使用范围 0，全员 1，指定人员（业务关联） 2，均不可提交',
+  `process_state` tinyint(1) NOT NULL DEFAULT '1' COMMENT '流程状态 0，不可用 1，可用 2，历史版本',
+  `model_content` text COMMENT '流程模型定义JSON内容',
+  `sort` int(11) DEFAULT '0' COMMENT '排序ID',
   `crt_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `crt_user` varchar(32) NOT NULL COMMENT '创建用户ID',
   `crt_name` varchar(255) NOT NULL COMMENT '创建用户',
