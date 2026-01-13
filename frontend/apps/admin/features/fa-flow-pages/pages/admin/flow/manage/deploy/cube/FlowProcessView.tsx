f804a33b44c01559a26a9fefc037db58e7bedce3
xu.pengfei <1508075252@qq.com>
2026-01-13 17:31:12 +0800

    1

--- frontend/apps/admin/features/fa-flow-pages/pages/admin/flow/manage/deploy/cube/FlowProcessView.tsx
+++ frontend/apps/admin/features/fa-flow-pages/pages/admin/flow/manage/deploy/cube/FlowProcessView.tsx
@@ -37,7 +37,7 @@ export default function FlowProcessView({ item }: FlowProcessViewProps) {
           <FlowProcessEdit item={item} viewOnly />
         </FaLazyContainer>
         <FaLazyContainer showCond={tab === 'workflow'}>
-          <FaWorkFlow processModel={JSON.parse(item.modelContent)} readOnly />
+          <FaWorkFlow flowProcess={item} processModel={JSON.parse(item.modelContent)} readOnly />
         </FaLazyContainer>
       </FaFlexRestLayout>
     </div>
