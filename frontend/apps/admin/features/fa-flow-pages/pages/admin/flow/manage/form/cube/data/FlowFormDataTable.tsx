87a4386a1f6541c50e94373c0808013daf21ea70
xu.pengfei <1508075252@qq.com>
2026-02-01 22:23:48 +0800

    feat: 新增流程表单数据管理表格组件。

--- frontend/apps/admin/features/fa-flow-pages/pages/admin/flow/manage/form/cube/data/FlowFormDataTable.tsx
+++ frontend/apps/admin/features/fa-flow-pages/pages/admin/flow/manage/form/cube/data/FlowFormDataTable.tsx
@@ -1,7 +1,7 @@
 import { flowFormApi } from '@/services';
 import { Flow } from '@/types';
 import { DownloadOutlined, EditOutlined, EyeOutlined, PlusOutlined, SearchOutlined } from '@ant-design/icons';
-import { AuthDelBtn, BaseBizTable, BaseTableUtils, clearForm, FaberTable, FaHref, useDelete, useTableQueryParams } from '@fa/ui';
+import { AuthDelBtn, BaseBizTable, BaseTableUtils, clearForm, FaberTable, FaHref, useDelete, useTableQueryParams, FaUtils } from '@fa/ui';
 import { Button, Form, Input, Space } from 'antd';
 import { each } from 'lodash';
 import React from 'react';
@@ -31,12 +31,13 @@ export default function FlowFormDataTable({ flowForm }: FlowFormDataTableProps) 
     ] as FaberTable.ColumnsProp<any>[];
 
     each(flowForm.tableConfig.table.columns, col => {
+      const dataIndex = FaUtils.toHump(col.field);
       if ('date' === col.dataType) {
-        columns.push(BaseTableUtils.genDateSorterColumn(col.label || col.field, col.field, col.width, sorter))
+        columns.push(BaseTableUtils.genDateSorterColumn(col.label || col.field, dataIndex, col.width, sorter))
       } else if ('datetime' === col.dataType) {
-        columns.push(BaseTableUtils.genTimeSorterColumn(col.label || col.field, col.field, col.width, sorter))
+        columns.push(BaseTableUtils.genTimeSorterColumn(col.label || col.field, dataIndex, col.width, sorter))
       } else {
-        columns.push(BaseTableUtils.genSimpleSorterColumn(col.label || col.field, col.field, col.width, sorter))
+        columns.push(BaseTableUtils.genSimpleSorterColumn(col.label || col.field, dataIndex, col.width, sorter))
       }
     })
 
