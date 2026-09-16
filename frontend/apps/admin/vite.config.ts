33c6bc1c90818eca058e8f07093cbc4d7f527b4d
xu.pengfei <1508075252@qq.com>
2026-09-16 15:29:47 +0800

    feat: enable Office file preview

--- frontend/apps/admin/vite.config.ts
+++ frontend/apps/admin/vite.config.ts
@@ -1,3 +1,4 @@
+import { fileViewerRenderers } from '@file-viewer/vite-plugin';
 import { defineConfig, loadEnv } from 'vite';
 // import react from '@vitejs/plugin-react-swc';
 import react from '@vitejs/plugin-react';
@@ -43,6 +44,7 @@ export default defineConfig(({ mode }) => {
       //   // 静态js、css文件的路径拼接规则
       //   prodUrl: '/plugins/{name}/{version}/{path}',
       // }),
+      fileViewerRenderers({ preset: 'office', copyAssets: true }),
       UnoCSS(),
       react(),
       Pages({
